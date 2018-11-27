b1a78cd07743df0cd6c58db05b672a641bcd3105
seconoid <seconsecond@gmail.com>
2018-11-27 11:29:19 +0900

    色々導入

--- resources/views/dev/index.blade.php
+++ resources/views/dev/index.blade.php
@@ -1,1 +1,21 @@
-こんうぃた！
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+  <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
+  <title>ちょわ</title>
+</head>
+<body>
+<h1>マークアップ</h1>
+<textarea id="editor" name="name" rows="8" cols="40"></textarea>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+<script>
+  var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+</script>
+</body>
+</html>
