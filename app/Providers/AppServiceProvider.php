656b4d3f4f1c95030e5372f4ed901da564cd52a6
seconoid <seconsecond@gmail.com>
2018-11-27 15:47:02 +0900

    set app env for dusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Dusk, if env is appropriate
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 }
