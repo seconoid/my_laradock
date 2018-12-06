0d4624f6c6e4804259c7a0ccfbfec733e7ef412e
seconoid <seconsecond@gmail.com>
2018-12-06 15:27:01 +0900

    fix dusk

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -16,8 +16,8 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/devs')
-                    ->assertSee('マーク');
+            $browser->visit('/')
+                    ->assertSee('Laravel');
         });
     }
 }
